f1ed203754773fdadb45feea19627cd05ff80954
snolbo <snorreob@stud.ntnu.no>
2017-02-21 19:40:31 +0100

    Finished

--- TTM4100 KTM/programming labs/lab3/SMTP.py
+++ TTM4100 KTM/programming labs/lab3/SMTP.py
@@ -20,67 +20,72 @@ clientSocket.connect((mailserver,serverPort))
 recv = clientSocket.recv(1024)
 print(recv)
 if recv[:3] != '220':
-	print('220 reply not received from server.')
+	print('220 reply not received from server on connect.')
 
 
 # Send HELO command and print server response.
-heloCommand = 'HELO Alice\r\n'
+heloCommand = 'HELO Alice\r\n'.encode('utf-8')
 clientSocket.send(heloCommand)
 recv1 = clientSocket.recv(1024)
 print(recv1)
 if recv1[:3] != '250':
-	print('250 reply not received from server.')
+	print('250 reply not received from server when HELO.')
+	
 
 # Send MAIL FROM command and print server response.
 # Fill in start
-clientSocket.send("MAIL FROM:snorreob@ntnu.no\r\n")
+clientSocket.send("MAIL FROM:snorreob@stud.ntnu.no\r\n".encode('utf-8'))
 recv3 = clientSocket.recv(1024)
 print(recv3)
 if recv3[:3] != '250':
-	print('250 reply not received from server.')
+	print('250 reply not received from server when MAIL FROM.')
 # Fill in end
 
 # Send RCPT TO command and print server response.
 # Fill in start
-clientSocket.send("RCPT To: snorreob@stud.ntnu.no\r\n")
+clientSocket.send("RCPT To: snorreob@stud.ntnu.no\r\n".encode('utf-8'))
 recv4 = clientSocket.recv(1024)
 print(recv4)
 if recv4[:3] != '250':
-	print('250 reply not received from server.')
+	print('250 reply not received from server when RCPT TO.')
 # Fill in end
 
 # Send DATA command and print server response.
 # Fill in start
-clientSocket.send("DATA\r\n")
+clientSocket.send("DATA\r\n".encode('utf-8'))
 recv5 = clientSocket.recv(1024)
 print(recv5)
 if(recv5[:3] != '354'):
-	print('354 reply not recieved from server.')
+	print('354 reply not recieved from server when DATA.')
 # Fill in end
 
 # Send message data.
 # Fill in start
-clientSocket.send("From: Snorre test snorreob@stud.ntnu.no\r\n")
-clientSocket.send("To: Snorre snorreob@ntnu.no\r\n")
-clientSocket.send("Cc: SB snorrebortnes@gmail.com\r\n")
-clientSocket.send("Date: today\r\n")
-clientSocket.send("Suvject: test message\r\n")
-clientSocket.send(" \r\n")
-clientSocket.send("Hello Snorre\r\n")
-clientSocket.send("Sending a test message usiong SMPT protocoll\r\n")
-clientSocket.send("Snorre\r\n")
+clientSocket.send("From: Snorre test snorreob@stud.ntnu.no\r\n".encode('utf-8'))
+clientSocket.send("To: Snorre snorreob@ntnu.no\r\n".encode('utf-8'))
+clientSocket.send("Cc: SB snorrebortnes@gmail.com\r\n".encode('utf-8'))
+clientSocket.send("Date: today\r\n".encode('utf-8'))
+clientSocket.send("Subject: test message\r\n".encode('utf-8'))
+clientSocket.send(" \r\n".encode('utf-8'))
+clientSocket.send("Hello Snorre\r\n".encode('utf-8'))
+clientSocket.send("Sending a test message using SMPT protocoll\r\n".encode('utf-8'))
+clientSocket.send("Snorre\r\n".encode('utf-8'))
 # Fill in end
 # Message ends with a single period.
 # Fill in start
-clientSocket.send("\r\n.\r\n")
+clientSocket.send("\r\n.\r\n".encode('utf-8'))
 recv6 = clientSocket.recv(1024)
 print(recv6)
 if(recv6[:3] != '250'):
-	print('250 reply not recieved from server.')
+	print('250 reply not recieved from server when ENDING DATA.')
 
 # Fill in end
 
 # Send QUIT command and get server response.
 # Fill in start
-clientSocket.send("QUITS: 221 Bye")
+clientSocket.send("QUIT: 221 Bye".encode('utf-8'))
+recv7 = clientSocket.recv(1024)
+print(recv7)
+if(recv7[:3] != '221'):
+	print('221 reply not recieved from server when QUIT.')
 # Fill in end
